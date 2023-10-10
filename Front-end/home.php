200ffcb5f84ad15d074a9746c76e0b97a1f7d3e8
MuhamadJaan <muhammadbaloch3437@gmail.com>
2023-10-10 09:50:54 +0500

    show home

---
+++ Front-end/home.php
@@ -0,0 +1,155 @@
+<?php
+include('BOOK-STORE-PHP/Front-end/Layouts/header.php')
+  ?>
+<?php
+include('BOOK-STORE-PHP/Front-end/Layouts/navbar.php')
+  ?>
+<div class="container mt-5">
+  <div class="row">
+    <div class="col-md-6 col-sm-12 mt-5">
+      <h1 class="mt-5 display-3 fw-bold w-75">The road to nowhere</h1>
+      <h4 class="mt-5 h3 w-75 ">
+        Lorem ipsum dolor sit amet, consectetur adipisicin elit, sed do eiusmod tempor incididunt
+        ut labore
+        et dolore magna aliqua.
+      </h4>
+      <span class="mt-3">_______</span>
+      <p class="mt-5 w-75 h6">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+        fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt <br>
+        mollit
+        anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+        doloremque laudantium.</p>
+      <button class="p-2 w-25 mt-3 bg-dark text-light">Read more</button>
+    </div>
+    <div class="col-md-6 col-12  mt-2 ">
+      <img src="BOOK-STORE-PHP/Front-end/images/k1.png" alt="" width="99%">
+    </div>
+  </div>
+</div>
+<div class="container">
+  <div class="row">
+    <div class="col-md-12 col-sm-12 mt-5 text-center">
+      <h1 class="fw-normal text-center ">Buy the book</h1>
+      <p class=" text-center w-100 ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit <br>
+        luctus nec ullamcorper
+        mattis, pulvinar dapibus leo.</p>
+    </div>
+  </div>
+</div>
+<div class="container-fluid  bg-light mt-5">
+  <div class="row align-content-lg-center">
+    <div class="col-md-6 col-sm-12 mt-5 text-center">
+      <img src="BOOK-STORE-PHP/Front-end/images/k2.png" alt="">
+    </div>
+    <div class="col-md-6 col-sm-12 mt-5">
+      <h1 class="fw-bolder mt-5 ">About the author</h1>
+      <h2 class="fw-normal mt-5 w-75 ">Culpa qui officia deserunt mollit anim id est laborum. Sed ut
+        perspiciatis unde omnis.</h2>
+      <span class="mt-5">_______</span>
+      <p class="mt-5 w-75">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+        fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+        accusantium doloremque laudantium.</p>
+      <span class="text-success fw-bold mt-5">Read More</span>
+    </div>
+  </div>
+</div>
+<div class="container-fluid mt-5">
+  <div class="row text-center">
+    <h4 class="mt-5 text-center fw-normal">Victor Hanse comes with his most deeply personal ipsum <br> dolor sit
+      amet, consectetur adipiscing elit. Ut
+      elit tellus <br> luctus nec ullamcorper mattis, pulvinar dapibus leo. Mauris <br> in erat justo.</h4>
+    <h4 class="mt-3 text-center fw-normal">Kristine Cox @ Premium e-Books</h4>
+    <div class="d-flex justify-content-center text-warning  mt-2">
+      <i class="bi bi-star-fill"></i>
+      <i class="bi bi-star-fill"></i>
+      <i class="bi bi-star-fill"></i>
+      <i class="bi bi-star-fill"></i>
+      <i class="bi bi-star-fill"></i>
+      <i class="bi bi-star-fill"></i>
+    </div>
+  </div>
+</div>
+<div class="container-fluid bg-light">
+  <div class="container mt-3 ">
+    <div class="row">
+      <div class="cols-md-12 col-sm-12 ">
+        <div class="bg text-center ">
+          <img class="mt-5 " src="BOOK-STORE-PHP/Front-end/images/k3.png" alt="" width="95%">
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="container mt-5">
+  <div class="row mt-5">
+    <div class="cols-md-4 col-sm-4 text-center">
+      <span class="mt-5 ">_______</span>
+      <h3 class="mt-3">paperback</h3>
+      <h4 class="fw-normal">$15.00 + shipping</h4>
+      <p class="fw-bold text-success mt-4">red more</p>
+    </div>
+    <div class="cols-md-4 col-sm-4 text-center">
+      <span class="mt-5">_______</span>
+      <h3 class="mt-3">paperback</h3>
+      <h4 class="fw-normal">$12.00 + shipping</h4>
+      <p class="fw-bold text-success mt-4">red more</p>
+    </div>
+    <div class="cols-md-4 col-sm-4 text-center">
+      <span class="mt-5">_______</span>
+      <h3 class="mt-3">paperback</h3>
+      <h4 class="fw-normal">$14.00 + shipping</h4>
+      <p class="fw-bold text-success mt-4">red more</p>
+    </div>
+  </div>
+</div>
+<div class="container">
+  <div class="row ">
+    <div class="col-md-12 col-sm-12 text-center">
+      <h1 class="mt-5">You'll also love</h1>
+      <p class="mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, <br> luctus
+        nec ullamcorper
+        mattis, pulvinar dapibus leo.</p>
+      <span>_______</span>
+    </div>
+  </div>
+</div>
+<div class="container ">
+  <div class="row mt-5">
+    <div class="col-md-4 mt-5">
+      <img src="BOOK-STORE-PHP/Front-end/images/k4.png" alt="">
+      <h1>Lines from poems</h1>
+      <h4>Victor Hansen</h4>
+      <span>$20.00</span>
+      <span>_______</span>
+      <br>
+      <input type="number"> <br>
+      <a href="#"><button class="mt-3 rounded-pill bg-success text-light">Add to cart</button></a>
+    </div>
+    <div class="col-md-4 mt-5">
+      <img src="BOOK-STORE-PHP/Front-end/images/k5.png" alt="">
+      <h1>line from Victor jack</h1>
+      <h4>Victor jack</h4>
+      <span>$10.00</span>
+      <span>_______</span>
+      <br>
+      <input type="number"><br>
+      <a href="#"> <button class="mt-3 rounded-pill bg-success text-light">Add to cart</button></a>
+    </div>
+    <div class="col-md-4 mt-5">
+      <img src="BOOK-STORE-PHP/Front-end/images/k6.png" alt="">
+      <h1>Lines from jassan</h1>
+      <h4>Victor jassan roye </h4>
+      <span>$12.00</span>
+      <span>_______</span>
+      <br>
+      <input type="number"> <br>
+      <a href="#"><button class="mt-3 rounded-pill bg-success text-light">Add to cart</button></a>
+    </div>
+  </div>
+</div>
+<?php
+include('BOOK-STORE-PHP/Front-end/Layouts/footer.php')
+  ?>
